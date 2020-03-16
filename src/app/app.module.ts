6fde499a057d156ea3b791aad212e21fcec425cf
voducdan <voducdand99@gmail.com>
2020-03-16 09:15:08 +0700

    Add: Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProductComponent } from "./product/product.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { HomeComponent } from './home/home.component';
 
 const appRoute = [
+  { path: "", component: HomeComponent },
   { path: "products", component: ProductComponent },
   { path: "products/:id", component: ProductDetailComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, ProductDetailComponent],
+  declarations: [AppComponent, ProductComponent, ProductDetailComponent, HomeComponent],
   imports: [
     RouterModule.forRoot(appRoute),
     BrowserModule,
